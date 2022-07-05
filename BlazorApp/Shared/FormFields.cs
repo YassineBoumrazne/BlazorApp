a1d12c40968799c353fdb4f5af0f42d90ee15ec0
Yassine Boumrazne <yassineboumrazne@gmail.com>
2022-07-05 02:04:52 +0100

    Add project files.

---
+++ BlazorApp/Shared/FormFields.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorApp.Shared
+{
+    public class FormFields
+    {
+        [Required]
+        public string Field1 { get; set; }
+
+        [Required]
+        public string Field2 { get; set; }
+
+        [Required]
+        public DateTime? Field3 { get; set; }
+
+        [Required]
+        public int Field4 { get; set; }
+
+        [Required]
+        public string Field5 { get; set; }
+
+        [Required]
+        public string Field6 { get; set; }
+
+        [Required]
+        public int Field7 { get; set; }
+
+        [Required]
+        public int Field8 { get; set; }
+
+        [Required]
+        public int Field9 { get; set; }
+
+        [Required]
+        public int Field10 { get; set; }
+
+        [Required]
+        public string Field11 { get; set; }
+    }
+}
